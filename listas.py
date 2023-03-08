e77452216721405fcdddd61e54cc49ef7f6b5be5
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-03-08 23:29:21 +0100

    Añadido un poco de explicación de listas

--- listas.py
+++ listas.py
@@ -4,3 +4,14 @@ mi_lista = list()
 mi_lista = [3,13,69,616]
 
 print(mi_lista)
+print(len(mi_lista))
+
+#Permite crear listas de objetos directamente
+mi_otra_lista = [29, 1.69, "Pache", "Devs"]
+
+print(mi_otra_lista)
+print(type(mi_otra_lista))
+
+#[N] permite ver el elemento N de la lista
+#Si ponemos números negativos mirará desde el último que es [-1]
+print(mi_otra_lista[0])

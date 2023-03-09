93a10391244dfe6f868a55d439566eede7f5598a
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-03-09 19:15:00 +0100

    Añadida más información de listas

--- listas.py
+++ listas.py
@@ -1,7 +1,7 @@
 # Aquí vamos a aprender sobre las listas
 
 mi_lista = list()
-mi_lista = [3,13,69,616]
+mi_lista = [3,13,69,616,3,17,3]
 
 print(mi_lista)
 print(len(mi_lista))
@@ -15,3 +15,11 @@ print(type(mi_otra_lista))
 #[N] permite ver el elemento N de la lista
 #Si ponemos números negativos mirará desde el último que es [-1]
 print(mi_otra_lista[0])
+#Count te dice cuantas veces se encuentra el elemento en la lista
+print(mi_lista.count(3))
+
+#Igualamos cada elemento de la lista a un variable
+#Importante el orden
+edad, altura, nombre, tipo = mi_otra_lista
+
+print(edad)
